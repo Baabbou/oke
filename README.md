87f1455476a6e217727ab5b4f9657b85c3976683
baabbou <theo.klingler1@etu.unilim.fr>
2024-09-06 13:21:28 +0200

    added full-type string encoding

--- README.md
+++ README.md
@@ -20,65 +20,77 @@ sudo scripts/uninstall.sh
 
 # Use
 
-## Basic char
-
 ```bash
-oke A
-+---------------+-----------+
-|    Format     |   Value   |
-+---------------+-----------+
-|    decimal    |    65     |
-|  hexadecimal  |   0x41    |
-|     byte      | 01000001  |
-|      URL      |    %41    |
-|   HTML name   |   None    |
-|   HTML dec    |   &#41;   |
-| HTML URL(dec) | %26%2341; |
-|   HTML hex    |  &#x41;   |
-| HTML URL(hex) | %26%2341; |
-|  Js unicode   |  \u0041   |
-|   URL 2ble    |   %2541   |
-+---------------+-----------+
+usage: oke [-h] [-c] [-d] [-x] [-b] [-f FULL_TYPE] value [value ...]
+
+OKÉ: A tool to convert anything into anything.
+
+positional arguments:
+  value                 The value you need to encode (as a char by default).
+
+options:
+  -h, --help            show this help message and exit
+  -c, --char            The given input is a char.
+  -d, --dec             The given input is a decimal.
+  -x, --hex             The given input is a hex.
+  -b, --byte            The given input is a byte.
+  -f FULL_TYPE, --full-type FULL_TYPE
+                        Convert a whole text and precise the type.
 ```
 
-## Decimal value
+## Basics char
 
-```bash
-oke -d 123
-+-------------+----------+
-|   Format    |  Value   |
-+-------------+----------+
-|   decimal   |   123    |
-| hexadecimal |   0x7b   |
-|    byte     | 01111011 |
-+-------------+----------+
+You will get all the forms of encoding in once for a single char :
+
+```
+oke 'A'
++------+-----------------+--------------+
+| Type |     Format      |    Value     |
++------+-----------------+--------------+
+|  0   |      given      |      A       |
+|  1   |     decimal     |      65      |
+|  2   |   hexadecimal   |     0x41     |
+|  3   |      byte       |   01000001   |
+|  4   |       URL       |     %41      |
+|  5   |   HTML named    |     None     |
+|  6   |    HTML num     |    &#65;     |
+|  7   |    HTML hex     |    &#x41;    |
+|  8   | URL(HTML named) |     None     |
+|  9   |  URL(HTML num)  | %26%2365%3b  |
+|  10  |  URL(HTML hex)  | %26%23x41%3b |
+|  11  |     unicode     |    \u0041    |
+|  12  |      URL2       |    %2541     |
++------+-----------------+--------------+
 ```
 
-## Hex value
+You can also precise another form of encoding as input (here in decimal) :
 
 ```bash
-oke -h 0x10
-+-------------+----------+
-|   Format    |  Value   |
-+-------------+----------+
-|   decimal   |    10    |
-| hexadecimal |   0x0a   |
-|    byte     | 00001010 |
-+-------------+----------+
+oke -d 65
++------+-------------+----------+
+| Type |   Format    |  Value   |
++------+-------------+----------+
+|  1   |    ASCII    |    A     |
+|  2   |   decimal   |    65    |
+|  3   | hexadecimal |   0x41   |
+|  4   |    byte     | 01000001 |
++------+-------------+----------+
 ```
+> Also avaliable in hex or in binary
 
-## Binary value
+## Encode a string
+
+```
+oke '<img src=1 onerror=alert(1)>' --full-type 6
+&#60;&#105;&#109;&#103;&#32;&#115;&#114;&#99;&#61;&#49;&#32;&#111;&#110;&#101;&#114;&#114;&#111;&#114;&#61;&#97;&#108;&#101;&#114;&#116;&#40;&#49;&#41;&#62;
+```
+> Precise the type using the left column when `oke A` (here type 6 is for `HTML num`).
 
 ```bash
-oke -b 1011
-+-------------+----------+
-|   Format    |  Value   |
-+-------------+----------+
-|   decimal   |    11    |
-| hexadecimal |   0x0b   |
-|    byte     | 00001011 |
-+-------------+----------+
+oke 'salut les nazes' -f 4
+%73%61%6c%75%74%20%6c%65%73%20%6e%61%7a%65%73
 ```
+> Here full-type 4 is for URL encode.
 
 # Contact
 
--- oke.py
+++ oke.py
@@ -12,76 +12,122 @@ parser.add_argument("-c", "--char", help="The given input is a char.", action="s
 parser.add_argument("-d", "--dec", help="The given input is a decimal.", action="store_true")
 parser.add_argument("-x", "--hex", help="The given input is a hex.", action="store_true")
 parser.add_argument("-b", "--byte", help="The given input is a byte.", action="store_true")
+
+parser.add_argument("-f", "--full-type", help="Convert a whole text and precise the type.")
+
 args = parser.parse_args()
 
 
-def f(val: str) -> str:
+def blue(val: str) -> str:
     return f"{Fore.BLUE}{val}{Style.RESET_ALL}"
 
 
 def translate_char(value: str) -> None:
     translations = []
-    translations.append([f("given"), value])
-    translations.append([f("decimal"), f"{ord(value)}"])
-    translations.append([f("hexadecimal"), "0x{:02x}".format(ord(value))])
-    translations.append([f("byte"), "{:08b}".format(ord(value))])
-    translations.append([f("URL"), "%{:02x}".format(ord(value))])
-    translations.append([f("HTML name"), html_entities_dict[value] if value in html_entities_dict else "None"])
-    translations.append([f("HTML dec"), "&#{:02x};".format(ord(value))])
-    translations.append([f("HTML URL(dec)"), "%26%23{:02x};".format(ord(value))])
-    translations.append([f("HTML hex"), "&#x{:02x};".format(ord(value))])
-    translations.append([f("HTML URL(hex)"), "%26%23{:02x};".format(ord(value))])
-    translations.append([f("Js unicode"), "\\u{:04x}".format(ord(value))])
-    translations.append([f("URL 2ble"), "%25{:02x}".format(ord(value))])
-    print(tabulate(translations, headers=[f("Format"), "Value"], tablefmt="pretty"))
+    translations.append([0, blue("given"), value])
+    translations.append([1, blue("decimal"), f"{ord(value)}"])
+    translations.append([2, blue("hexadecimal"), "0x{:02x}".format(ord(value))])
+    translations.append([3, blue("byte"), "{:08b}".format(ord(value))])
+    translations.append([4, blue("URL"), "%{:02x}".format(ord(value))])
+    translations.append([5, blue("HTML named"), html_entities_dict[value] if value in html_entities_dict else "None"])
+    translations.append([6, blue("HTML num"), f"&#{ord(value)};"])
+    translations.append([7, blue("HTML hex"), "&#x{:02x};".format(ord(value))])
+    translations.append([8, blue("URL(HTML named)"), html_entities_dict[value].replace("&", "%26").replace(";", "%3b") if value in html_entities_dict else "None"])
+    translations.append([9, blue("URL(HTML num)"), f"%26%23{ord(value)}%3b"])
+    translations.append([10, blue("URL(HTML hex)"), "%26%23x{:02x}%3b".format(ord(value))])
+    translations.append([11, blue("unicode"), "\\u{:04x}".format(ord(value))])
+    translations.append([12, blue("URL2"), "%25{:02x}".format(ord(value))])
+    print(tabulate(translations, headers=["Type", blue("Format"), "Value"], tablefmt="pretty"))
 
 
 def translate_dec(value: int) -> None:
     translations = []
-    translations.append([f("decimal"), f"{value}"])
-    translations.append([f("hexadecimal"), "0x{:02x}".format(value)])
-    translations.append([f("byte"), "{:08b}".format(value)])
-    print(tabulate(translations, headers=[f("Format"), "Value"], tablefmt="pretty"))
+    translations.append([1, blue("ASCII"), chr(value)])
+    translations.append([2, blue("decimal"), f"{value}"])
+    translations.append([3, blue("hexadecimal"), "0x{:02x}".format(value)])
+    translations.append([4, blue("byte"), "{:08b}".format(value)])
+    print(tabulate(translations, headers=["Type", blue("Format"), "Value"], tablefmt="pretty"))
 
 
 if __name__ == '__main__':
-    for value in args.value:
-        try:
-            if args.char:
-                if len(value) != 1:
-                    print("[error] Give it char by char pls.")
-                    exit(1)
-                translate_char(value)
 
-            elif args.dec:
-                try:
-                    translate_dec(int(value, 10))
-                except ValueError:
-                    print("[error] You didn't gave a valid decimal value.")
-                    exit(1)
+    if args.full_type:
+        value = args.value[0]
+        if args.full_type == "1":
+            translation = " ".join([f"{ord(c)}" for c in value])
 
-            elif args.hex:
-                try:
-                    translate_dec(int(value, 16))
-                except ValueError:
-                    print("[error] You didn't gave a valid hex value.")
-                    exit(1)
+        elif args.full_type == "2":
+            translation = " ".join(["0x{:02x}".format(ord(c)) for c in value])
 
-            elif args.byte:
-                try:
-                    translate_dec(int(value, 2))
-                except ValueError:
-                    print("[error] You didn't gave a valid byte.")
-                    exit(1)
+        elif args.full_type == "3":
+            translation = " ".join(["{:08b}".format(ord(c)) for c in value])
 
-            else:
-                if len(value) != 1:
-                    print("[error] This tool can handle only 1 char...")
-                    exit(1)
-                translate_char(value)
-        except Exception as e:
-            print("[error] An error occured please specify the right type for your input.")
-            exit(1)
+        elif args.full_type == "4":
+            translation = "".join(["%{:02x}".format(ord(c)) for c in value])
 
-        if (args.value).index(value) != len(args.value)-1:
-            print()
+        elif args.full_type == "5":
+            translation = "".join([html_entities_dict[c] if c in html_entities_dict else "&#x{:02x};".format(ord(c)) for c in value])
+
+        elif args.full_type == "6":
+            translation = "".join([f"&#{ord(c)};" for c in value])
+
+        elif args.full_type == "7":
+            translation = "".join(["&#x{:02x};".format(ord(c)) for c in value])
+
+        elif args.full_type == "8":
+            translation = "".join([(html_entities_dict[c] if c in html_entities_dict else "&#x{:02x};".format(ord(c))).replace("&", "%26").replace("#", "%23").replace(";", "%3b") for c in value])
+
+        elif args.full_type == "7":
+            translation = "".join([f"%26%23{ord(c)}%3b" for c in value])
+
+        elif args.full_type == "9":
+            translation = "".join(["%26%23x{:02x}%3b".format(ord(c)) for c in value])
+
+        elif args.full_type == "10":
+            translation = "".join(["\\u{:04x}".format(ord(c)) for c in value])
+
+        elif args.full_type == "11":
+            translation = "".join(["%25{:02x}".format(ord(c)) for c in value])
+
+        else:
+            translation = "".join(["%{:02x}".format(ord(c)) for c in value])
+        print(translation)
+
+    else:
+        for value in args.value:
+            try:
+                if args.char:
+                    if len(value) != 1:
+                        print("[error] Give it char by char pls.")
+                        exit(1)
+                    translate_char(value)
+
+                elif args.dec:
+                    try:
+                        translate_dec(int(value, 10))
+                    except ValueError:
+                        print("[error] You didn't gave a valid decimal value.")
+                        exit(1)
+
+                elif args.hex:
+                    try:
+                        translate_dec(int(value, 16))
+                    except ValueError:
+                        print("[error] You didn't gave a valid hex value.")
+                        exit(1)
+
+                elif args.byte:
+                    try:
+                        translate_dec(int(value, 2))
+                    except ValueError:
+                        print("[error] You didn't gave a valid byte.")
+                        exit(1)
+
+                else:
+                    if len(value) != 1:
+                        print("[error] This tool can handle only 1 char...")
+                        exit(1)
+                    translate_char(value)
+            except Exception as e:
+                print("[error] An error occured please specify the right type for your input.")
+                exit(1)
